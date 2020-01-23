eecb96da5d4b4b2be8dde10b8aa96c39147bc1d7
anmolmahatpurkar <anmol@cogoport.com>
2020-01-24 04:06:07 +0530

    PWA Theme Color

--- common/Meta.tsx
+++ common/Meta.tsx
@@ -28,11 +28,11 @@ const Meta: React.FC<MetaProps> = ({ title, description, author, url }) => (
 		<link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
 		<link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
 		<link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
-		<link rel="mask-icon" href="/images/safari-pinned-tab.svg" color="#6c5ac0" />
+		<link rel="mask-icon" href="/images/safari-pinned-tab.svg" color="#764FB7" />
 		<link rel="manifest" href="/manifest.json" />
 
-		<meta name="msapplication-TileColor" content="#603cba" />
-		<meta name="theme-color" content="#603cba" />
+		<meta name="msapplication-TileColor" content="#764FB7" />
+		<meta name="theme-color" content="#764FB7" />
 	</Head>
 );
 
